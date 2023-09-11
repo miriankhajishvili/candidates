ec79d6b47ebc42387351099c9e407b4c9f4e4f9e
Mirian_Khajishvili <mirian.khajishvili@gmail.comm>
2023-09-11 16:37:22 +0400

    add refresh to delete

--- src/app/candidates/candidates.component.ts
+++ src/app/candidates/candidates.component.ts
@@ -1,32 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component,  } from '@angular/core';
 import {  Router } from '@angular/router';
 import { CandidatesService } from '../features/services/candidates.service';
+import { of } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-candidates',
   templateUrl: './candidates.component.html',
-  styleUrls: ['./candidates.component.scss']
+  styleUrls: ['./candidates.component.scss'],
+
+  
 })
 export class CandidatesComponent  {
 
   constructor( 
     private candidatesService: CandidatesService,
-    private router: Router){}
+    private router: Router,){}
 
 
   allCandidates$ = this.candidatesService.getCandidates()
 
 
+
+  onDelete(id: any) {
+    this.candidatesService.deleteCandidate(id).subscribe((res) => {
+
+      this.candidatesService.getCandidates().subscribe((updatedCandidates) => {
+     
+        this.allCandidates$ = of(updatedCandidates);
+      });
   
-
-  onDelete(id: any){
-
-    this.candidatesService.deleteCandidate(id).subscribe(res => {
-      console.log(res)
-      this.router.navigate(['/candidates']);
-    })
+     
+    });
 
   }
 
 }
+
+
--- src/app/candidates/candidates.module.ts
+++ src/app/candidates/candidates.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CandidatesRoutingModule } from './candidates-routing.module';
 import { CandidatesComponent } from './candidates.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon'
-import {MatDividerModule} from '@angular/material/divider';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon'
+import { MatDividerModule} from '@angular/material/divider';
+import { MatDialogModule} from '@angular/material/dialog';
+
 
 
 
@@ -23,7 +25,9 @@ import {MatDividerModule} from '@angular/material/divider';
     
     MatButtonModule,
     MatIconModule,
-    MatDividerModule
+    MatDividerModule,
+    MatDialogModule
+
     
   ]
 })
